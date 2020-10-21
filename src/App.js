6a91569c827bfceea793790756b41d07fe20c4a8
Augusto <augstarachrafael@gmail.com>
2020-10-21 20:53:38 -0300

    Corrigindo versão eslint

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import './App.css';
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <h3>Running</h3>
+    </div>
+  );
 }
 
 export default App;
